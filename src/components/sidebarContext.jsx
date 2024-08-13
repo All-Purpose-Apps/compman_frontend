605687242f03732231bd2ab7187402f99fbd105d
joshpled <joshuapleduc@gmail.com>
2024-08-12 22:32:14 -0400

    remove useless sidebar context states

--- src/components/sidebarContext.jsx
+++ src/components/sidebarContext.jsx
@@ -6,18 +6,11 @@ const SidebarContext = createContext({});
 
 export const MyProSidebarProvider = ({ children }) => {
   const [sidebarRTL, setSidebarRTL] = useState(false);
-  const [sidebarBackgroundColor, setSidebarBackgroundColor] = useState(undefined);
-  const [sidebarImage, setSidebarImage] = useState(undefined);
+
   return (
     <ProSidebarProvider>
       <SidebarContext.Provider
         value={{
-          sidebarBackgroundColor,
-          setSidebarBackgroundColor,
-
-          sidebarImage,
-          setSidebarImage,
-
           sidebarRTL,
           setSidebarRTL,
         }}
