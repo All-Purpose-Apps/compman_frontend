153e6ec8023eb612e2a12af8d3118ce109629e67
joshpled <joshuapleduc@gmail.com>
2024-08-13 21:48:33 -0400

    change checkCouples to checkEntries

--- src/store/couplesSlice.js
+++ src/store/couplesSlice.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { checkCouples } from 'src/utils/checkCouples';
+import { checkCouples } from 'src/utils/createEntries';
 
 const initialState = {
   couples: [],
