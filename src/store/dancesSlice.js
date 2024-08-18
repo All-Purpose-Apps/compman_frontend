e87cec875be4e8aaabfc3c404b73519436d2be78
joshpled <joshuapleduc@gmail.com>
2024-08-18 19:35:45 -0400

    add danceCategories to danceSlices

--- src/store/dancesSlice.js
+++ src/store/dancesSlice.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const initialState = {
   dances: [],
+  danceCategories: [],
   status: 'idle',
   error: null,
 };
@@ -10,6 +11,15 @@ const initialState = {
 export const fetchDances = createAsyncThunk('dances/fetchDances', async () => {
   try {
     const response = await axios.get(`${import.meta.env.VITE_BACKEND_DEV}/dances`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+});
+
+export const fetchDanceCategories = createAsyncThunk('dances/fetchDanceCategories', async () => {
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_BACKEND_DEV}/danceCategory`);
     return response.data;
   } catch (error) {
     throw new Error(error.response.data.message);
@@ -30,6 +40,17 @@ export const dancesSlice = createSlice({
         state.dances = action.payload;
       })
       .addCase(fetchDances.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(fetchDanceCategories.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchDanceCategories.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.danceCategories = action.payload;
+      })
+      .addCase(fetchDanceCategories.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
       });
