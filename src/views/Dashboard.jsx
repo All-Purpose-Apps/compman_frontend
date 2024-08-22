c3534d44f7ff9d2051f4c137ad3a4f10ca273358
joshpled <joshuapleduc@gmail.com>
2024-08-22 00:13:32 -0400

    reformat dashboard and add next three heats table

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -4,11 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchDancers } from 'src/store/dancersSlice';
 import { fetchStudios } from 'src/store/studiosSlice';
 import { fetchHeats } from 'src/store/heatsSlice';
-import { Card, CardContent, Grid, Box, Typography, Paper, List, ListItem, ListItemText, Divider } from '@mui/material';
+import { Card, CardContent, Grid, Box, Typography, Paper, List, ListItem, ListItemText, Divider, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { fetchEntries } from 'src/store/entriesSlice';
-import { fetchSchedules } from 'src/store/schedulesSlice'; // Assuming this exists
+import { fetchSchedules } from 'src/store/schedulesSlice';
 import moment from 'moment';
-import { BarChart } from '@mui/x-charts/BarChart'; // Import the BarChart component from MUI X Charts
 import HomeIcon from '@mui/icons-material/Home';
 import PersonIcon from '@mui/icons-material/Person';
 import AssignmentIcon from '@mui/icons-material/Assignment';
@@ -23,25 +22,20 @@ export default function Dashboard() {
     const studios = useSelector(state => state.studios.studios);
     const heats = useSelector(state => state.heats.heats);
     const entries = useSelector(state => state.entries.entries);
-    const schedules = useSelector(state => state.schedules.schedules); // Assuming this exists
-
-    // Mock data for money collected, replace with your actual data
-    const moneyData = [
-        { month: 'January', amount: 4000 },
-        { month: 'February', amount: 3000 },
-        { month: 'March', amount: 5000 },
-        { month: 'April', amount: 7000 },
-        { month: 'May', amount: 2000 },
-        { month: 'June', amount: 6000 },
-    ];
+    const schedules = useSelector(state => state.schedules.schedules);
 
     useEffect(() => {
         dispatch(fetchDancers());
         dispatch(fetchStudios());
         dispatch(fetchHeats());
         dispatch(fetchEntries());
-        dispatch(fetchSchedules()); // Fetch schedules
+        dispatch(fetchSchedules());
     }, [dispatch]);
+
+    const upcomingHeats = heats
+        .filter(heat => moment(heat.dateTime).isAfter(moment()))
+        .sort((a, b) => moment(a.dateTime).diff(moment(b.dateTime)))
+        .slice(0, 3);
 
     return (
         <Box p={2}>
@@ -51,10 +45,10 @@ export default function Dashboard() {
                         <Card>
                             <CardContent style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} onClick={() => navigate('/admin/studios')} sx={{ cursor: 'pointer' }}>
                                 <Box textAlign="center">
-                                    <Typography variant="subtitle1">Total Studios</Typography>
-                                    <HomeIcon fontSize="large" />
+                                    <Typography variant="h5">Total Studios</Typography>
+                                    <HomeIcon sx={{ marginTop: '20px' }} fontSize="large" />
                                 </Box>
-                                <Typography variant="h4">{studios.length}</Typography>
+                                <Typography variant="h1">{studios.length}</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
@@ -62,10 +56,10 @@ export default function Dashboard() {
                         <Card>
                             <CardContent style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} onClick={() => navigate('/admin/dancers')} sx={{ cursor: 'pointer' }}>
                                 <Box textAlign="center">
-                                    <Typography variant="subtitle1">Total Dancers</Typography>
-                                    <PersonIcon fontSize="large" />
+                                    <Typography variant="h5">Total Dancers</Typography>
+                                    <PersonIcon sx={{ marginTop: '20px' }} fontSize="large" />
                                 </Box>
-                                <Typography variant="h4">{dancers.length}</Typography>
+                                <Typography variant="h1">{dancers.length}</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
@@ -73,10 +67,10 @@ export default function Dashboard() {
                         <Card>
                             <CardContent style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} onClick={() => navigate('/admin/entries')} sx={{ cursor: 'pointer' }}>
                                 <Box textAlign="center">
-                                    <Typography variant="subtitle1">Total Entries</Typography>
-                                    <AssignmentIcon fontSize="large" />
+                                    <Typography variant="h5">Total Entries</Typography>
+                                    <AssignmentIcon sx={{ marginTop: '20px' }} fontSize="large" />
                                 </Box>
-                                <Typography variant="h4">{entries.length}</Typography>
+                                <Typography variant="h1">{entries.length}</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
@@ -84,57 +78,96 @@ export default function Dashboard() {
                         <Card>
                             <CardContent style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} onClick={() => navigate('/admin/heats')} sx={{ cursor: 'pointer' }}>
                                 <Box textAlign="center">
-                                    <Typography variant="subtitle1">Total Heats</Typography>
-                                    <WhatshotIcon fontSize="large" />
+                                    <Typography variant="h5">Total Heats</Typography>
+                                    <WhatshotIcon sx={{ marginTop: '20px' }} fontSize="large" />
                                 </Box>
-                                <Typography variant="h4">{heats.length}</Typography>
+                                <Typography variant="h1">{heats.length}</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
                 </Grid>
             </Paper>
 
-            <Box mt={4}>
-                <Paper elevation={3} style={{ padding: '16px' }}>
-                    <Typography variant="h6" gutterBottom>Current Schedule</Typography>
-                    <List>
-                        {schedules.length === 0 ? (
-                            <Typography>No schedule available.</Typography>
-                        ) : (
-                            schedules.map((schedule, index) => (
-                                <React.Fragment key={schedule.id}>
-                                    <ListItem>
-                                        <ListItemText
-                                            primary={`${moment(schedule.startDate).format('MM/DD/YYYY - h:mm A')}`}
-                                            secondary={`Event: ${schedule.dances.map((dance) => `${dance.title} - ${dance.danceCategory.name}`)}, Location: ${schedule.location}`}
-                                        />
-                                    </ListItem>
-                                    {index < schedules.length - 1 && <Divider />}
-                                </React.Fragment>
-                            ))
-                        )}
-                    </List>
-                </Paper>
-            </Box>
-
-            {/* <Box mt={4}>
-                <Paper elevation={3} style={{ padding: '16px' }}>
-                    <Typography variant="h6" gutterBottom>Money Collected</Typography>
-                    <BarChart
-                        data={moneyData}
-                        xKey={['month']}
-                        yKeys={['amount']}
-                        series={[
-                            {
-                                data: [2, 5, 3],
-                            },
-                        ]}
-                        width={600}
-                        height={300}
-                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-                    />
-                </Paper>
-            </Box> */}
+            <Grid container spacing={2} mt={2}>
+                <Grid item xs={12} md={6}>
+                    <Paper elevation={3} style={{ padding: '16px' }}>
+                        <Typography variant="h3" gutterBottom>Current Schedule</Typography>
+                        <List>
+                            {schedules.length === 0 ? (
+                                <ListItem>
+                                    <Typography variant="body1">No schedule available.</Typography>
+                                </ListItem>
+                            ) : (
+                                schedules.map((schedule, index) => (
+                                    <React.Fragment key={schedule._id}>
+                                        <ListItem>
+                                            <ListItemText
+                                                primary={
+                                                    <Typography variant="h4" mb={2}>
+                                                        {moment(schedule.startDate).format('MMMM Do YYYY, h:mm A')}
+                                                    </Typography>
+                                                }
+                                                secondary={
+                                                    <Box display="flex" flexDirection="column" mt={1}>
+                                                        <Typography component="span" variant="h5">
+                                                            <strong>Event:</strong> {schedule.dances.map((dance) => `${dance.title} - ${dance.danceCategory.name}`).join(', ')}
+                                                        </Typography>
+                                                        <Typography component="span" variant="h5" mt={1}>
+                                                            <strong>Location:</strong> {schedule.location}
+                                                        </Typography>
+                                                    </Box>
+                                                }
+                                            />
+                                        </ListItem>
+                                        {index < schedules.length - 1 && <Divider />}
+                                    </React.Fragment>
+                                ))
+                            )}
+                        </List>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Paper elevation={3} style={{ padding: '16px' }}>
+                        <Typography variant="h3" gutterBottom>Next Three Heats</Typography>
+                        <TableContainer component={Paper}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Start Time</TableCell>
+                                        <TableCell>Dance</TableCell>
+                                        <TableCell>Entries</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {upcomingHeats.length === 0 ? (
+                                        <TableRow>
+                                            <TableCell colSpan={3} align="center">
+                                                No upcoming heats.
+                                            </TableCell>
+                                        </TableRow>
+                                    ) : (
+                                        upcomingHeats.map((heat) => (
+                                            <TableRow key={heat._id}>
+                                                <TableCell>{moment(heat.dateTime).format('MMMM Do YYYY, h:mm A')}</TableCell>
+                                                <TableCell>{heat.dance.title} - {heat.dance.danceCategory.name}</TableCell>
+                                                <TableCell>
+                                                    <List>
+                                                        {heat.entries.map((entry) => (
+                                                            <ListItem key={entry._id} disablePadding>
+                                                                <ListItemText primary={`${entry.leader.fullName} & ${entry.follower.fullName}`} />
+                                                            </ListItem>
+                                                        ))}
+                                                    </List>
+                                                </TableCell>
+                                            </TableRow>
+                                        ))
+                                    )}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Paper>
+                </Grid>
+            </Grid>
         </Box>
     );
 }
