fd8f84509f288e4b6143a103d27fdb7a85e1c402
joshpled <joshuapleduc@gmail.com>
2024-08-25 16:08:47 -0400

    Convert Edit Entry to use Formik

--- src/views/Entries/EditEntry.jsx
+++ src/views/Entries/EditEntry.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { Formik, Field, Form } from 'formik';
 import * as yup from 'yup';
 import { Box, Button, Typography, TextField, FormControl, CircularProgress, Paper, useTheme, Autocomplete, MenuItem } from '@mui/material';
 import { getOneEntry, editEntry } from 'src/store/entriesSlice';
@@ -28,36 +27,37 @@ export default function EditEntry() {
     const navigate = useNavigate();
     const { id } = useParams();
 
-    const { register, handleSubmit, setValue, formState: { errors } } = useForm({
-        resolver: yupResolver(schema)
+    const [initialValues, setInitialValues] = useState({
+        leader: '',
+        follower: '',
+        dance: '',
+        ageCategory: '',
+        level: '',
     });
 
     useEffect(() => {
         const fetchEntry = async () => {
             const response = await dispatch(getOneEntry(id));
             const entry = response.payload[0];
-            setValue('leader', entry.leader._id);
-            setValue('follower', entry.follower._id);
-            setValue('dance', entry.dance._id);
-            setValue('ageCategory', entry.ageCategory);
-            setValue('level', entry.level);
+            setInitialValues({
+                leader: entry.leader._id,
+                follower: entry.follower._id,
+                dance: entry.dance._id,
+                ageCategory: entry.ageCategory,
+                level: entry.level,
+            });
         };
 
         fetchEntry();
         dispatch(fetchDancers());
         dispatch(fetchDances());
-    }, [dispatch, id, setValue]);
+    }, [dispatch, id]);
 
     const entry = useSelector(state => state.entries.entry[0]);
     const dancers = useSelector(state => state.dancers.dancers);
     const dances = useSelector(state => state.dances.dances);
     const isLoading = useSelector(state => state.entries.status) === 'loading';
     const error = useSelector(state => state.entries.error);
-
-    const onSubmit = (data) => {
-        dispatch(editEntry({ id, ...data }));
-        navigate(-1);
-    };
 
     const handleCancel = () => {
         navigate(-1);
@@ -84,107 +84,119 @@ export default function EditEntry() {
     return (
         <Box sx={{ maxWidth: 600, mx: "auto", mt: 4 }}>
             <Paper elevation={3} sx={{ padding: 3, backgroundColor: colors.primary[400] }}>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <FormControl fullWidth margin="normal">
-                        <Autocomplete
-                            options={dancerOptions}
-                            getOptionLabel={(option) => option.label}
-                            defaultValue={dancerOptions.find(option => option.value === entry?.leader._id)}
-                            onChange={(e, value) => setValue('leader', value?.value || '')}
-                            renderInput={(params) => (
-                                <TextField
-                                    {...params}
-                                    label="Leader"
-                                    error={!!errors.leader}
-                                    helperText={errors.leader?.message}
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={schema}
+                    onSubmit={(values) => {
+                        dispatch(editEntry({ id, ...values }));
+                        navigate(-1);
+                    }}
+                    enableReinitialize
+                >
+                    {({ errors, touched, setFieldValue, values }) => (
+                        <Form>
+                            <FormControl fullWidth margin="normal">
+                                <Autocomplete
+                                    options={dancerOptions}
+                                    getOptionLabel={(option) => option.label}
+                                    value={dancerOptions.find(option => option.value === values.leader) || null}
+                                    onChange={(e, value) => setFieldValue('leader', value?.value || '')}
+                                    renderInput={(params) => (
+                                        <TextField
+                                            {...params}
+                                            label="Leader"
+                                            error={touched.leader && Boolean(errors.leader)}
+                                            helperText={touched.leader && errors.leader}
+                                        />
+                                    )}
                                 />
-                            )}
-                        />
-                    </FormControl>
+                            </FormControl>
 
-                    <FormControl fullWidth margin="normal">
-                        <Autocomplete
-                            options={dancerOptions}
-                            getOptionLabel={(option) => option.label}
-                            defaultValue={dancerOptions.find(option => option.value === entry?.follower._id)}
-                            onChange={(e, value) => setValue('follower', value?.value || '')}
-                            renderInput={(params) => (
-                                <TextField
-                                    {...params}
-                                    label="Follower"
-                                    error={!!errors.follower}
-                                    helperText={errors.follower?.message}
+                            <FormControl fullWidth margin="normal">
+                                <Autocomplete
+                                    options={dancerOptions}
+                                    getOptionLabel={(option) => option.label}
+                                    value={dancerOptions.find(option => option.value === values.follower) || null}
+                                    onChange={(e, value) => setFieldValue('follower', value?.value || '')}
+                                    renderInput={(params) => (
+                                        <TextField
+                                            {...params}
+                                            label="Follower"
+                                            error={touched.follower && Boolean(errors.follower)}
+                                            helperText={touched.follower && errors.follower}
+                                        />
+                                    )}
                                 />
-                            )}
-                        />
-                    </FormControl>
+                            </FormControl>
 
-                    <FormControl fullWidth margin="normal">
-                        <Autocomplete
-                            options={danceOptions}
-                            getOptionLabel={(option) => option.label}
-                            defaultValue={danceOptions.find(option => option.value === entry?.dance._id)}
-                            onChange={(e, value) => setValue('dance', value?.value || '')}
-                            renderInput={(params) => (
-                                <TextField
-                                    {...params}
-                                    label="Dance"
-                                    error={!!errors.dance}
-                                    helperText={errors.dance?.message}
+                            <FormControl fullWidth margin="normal">
+                                <Autocomplete
+                                    options={danceOptions}
+                                    getOptionLabel={(option) => option.label}
+                                    value={danceOptions.find(option => option.value === values.dance) || null}
+                                    onChange={(e, value) => setFieldValue('dance', value?.value || '')}
+                                    renderInput={(params) => (
+                                        <TextField
+                                            {...params}
+                                            label="Dance"
+                                            error={touched.dance && Boolean(errors.dance)}
+                                            helperText={touched.dance && errors.dance}
+                                        />
+                                    )}
                                 />
-                            )}
-                        />
-                    </FormControl>
+                            </FormControl>
 
-                    <FormControl fullWidth margin="normal">
-                        <TextField
-                            select
-                            label="Age Category"
-                            defaultValue={entry?.ageCategory || ''}
-                            {...register('ageCategory')}
-                            error={!!errors.ageCategory}
-                            helperText={errors.ageCategory?.message}
-                            SelectProps={{
-                                multiple: false,
-                            }}
-                        >
-                            {AGE_CATEGORIES.map(category => (
-                                <MenuItem key={category} value={category} sx={formSxSettings(colors)}>
-                                    {capitalizeWords(category)}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                    </FormControl>
+                            <FormControl fullWidth margin="normal">
+                                <Field
+                                    as={TextField}
+                                    select
+                                    label="Age Category"
+                                    name="ageCategory"
+                                    error={touched.ageCategory && Boolean(errors.ageCategory)}
+                                    helperText={touched.ageCategory && errors.ageCategory}
+                                    SelectProps={{
+                                        multiple: false,
+                                    }}
+                                >
+                                    {AGE_CATEGORIES.map(category => (
+                                        <MenuItem key={category} value={category} sx={formSxSettings(colors)}>
+                                            {capitalizeWords(category)}
+                                        </MenuItem>
+                                    ))}
+                                </Field>
+                            </FormControl>
 
-                    <FormControl fullWidth margin="normal">
-                        <TextField
-                            select
-                            label="Level"
-                            defaultValue={entry?.level || ''}
-                            {...register('level')}
-                            error={!!errors.level}
-                            helperText={errors.level?.message}
-                            SelectProps={{
-                                multiple: false,
-                            }}
-                        >
-                            {LEVELS.map(level => (
-                                <MenuItem key={level} value={level} sx={formSxSettings(colors)}>
-                                    {capitalizeWords(level)}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                    </FormControl>
+                            <FormControl fullWidth margin="normal">
+                                <Field
+                                    as={TextField}
+                                    select
+                                    label="Level"
+                                    name="level"
+                                    error={touched.level && Boolean(errors.level)}
+                                    helperText={touched.level && errors.level}
+                                    SelectProps={{
+                                        multiple: false,
+                                    }}
+                                >
+                                    {LEVELS.map(level => (
+                                        <MenuItem key={level} value={level} sx={formSxSettings(colors)}>
+                                            {capitalizeWords(level)}
+                                        </MenuItem>
+                                    ))}
+                                </Field>
+                            </FormControl>
 
-                    <Box mt={4} display="flex" justifyContent="space-between">
-                        <Button variant="contained" color="primary" type="submit">
-                            Save
-                        </Button>
-                        <Button variant="outlined" color="secondary" onClick={handleCancel}>
-                            Cancel
-                        </Button>
-                    </Box>
-                </form>
+                            <Box mt={4} display="flex" justifyContent="space-between">
+                                <Button variant="contained" color="primary" type="submit">
+                                    Save
+                                </Button>
+                                <Button variant="outlined" color="secondary" onClick={handleCancel}>
+                                    Cancel
+                                </Button>
+                            </Box>
+                        </Form>
+                    )}
+                </Formik>
             </Paper>
         </Box>
     );
